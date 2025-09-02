4c1cea39652421064251d8948557a7f9b2bb2add
Mushfiqur Rahman Salin <amisalin2@gmail.com>
2025-09-02 13:33:24 +0600

    Update Average_marks.cpp

--- Array_Of_Object/Average_marks.cpp
+++ Array_Of_Object/Average_marks.cpp
@@ -1,6 +1,6 @@
-//Write a program to print the roll number and average marks of 8 students in three
+/*Write a program to print the roll number and average marks of 8 students in three
 subjects (each out of 100). The marks are entered by the user and the roll numbers are
-automatically assigned
+automatically assigned */
 
 #include<iostream>
 #include<string>
